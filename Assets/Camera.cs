d24f30ebd26f09bfded358f6c3a9a9614b06f143
Dylan Pomeroy <dylan.pomeroy@redquantum.ca>
2018-01-27 16:50:20 -0400

    Milstone - got audio and video sync ups, setup methods for staging

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -1,27 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Assets;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class Camera : MonoBehaviour {
 
-    VideoPlayer videoPlayer;
-    private GameObject buttonPrefab;
+    VideoPlayer VideoPlayer;
+    AudioSource AudioPlayer;
     private List<GameObject> OptionButtons;
 
-
-    // Use this for initialization
-    void Start () {
-        var camera = GameObject.Find("Main Camera");
-        videoPlayer = camera.AddComponent<VideoPlayer>();
-        videoPlayer.playOnAwake = false;
-        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
-        videoPlayer.targetCameraAlpha = 0.99f;
-        videoPlayer.url = Application.dataPath + "/video3.mp4";
-        videoPlayer.isLooping = true;
-        videoPlayer.audioOutputMode = VideoAudioOutputMode.Direct;
-        videoPlayer.Play();
+    int CurrentStage;
+    int CurrentDialogLevel;
+    Stopwatch Stopwatch;
+    List<int> CurrentDialogOptionSelections;
+    
+    void Start() {
+        InitializeVideoPlayer();
+        PlayVideo();
 
         OptionButtons = new List<GameObject>()
         {
@@ -35,36 +33,204 @@ public class Camera : MonoBehaviour {
             optionButton.SetActive(false);
         });
         OptionButtons[0].GetComponent<Button>().onClick.AddListener(OptionAClicked);
+        OptionButtons[1].GetComponent<Button>().onClick.AddListener(OptionBClicked);
+        OptionButtons[2].GetComponent<Button>().onClick.AddListener(OptionCClicked);
+        OptionButtons[3].GetComponent<Button>().onClick.AddListener(OptionDClicked);
+
+        CurrentStage = 0;
+        CurrentDialogLevel = 0;
+        CurrentDialogOptionSelections = new List<int>();
+        Stopwatch = new Stopwatch();
     }
 
-    bool hitPoint0 = false;
-    bool hitPoint1 = false;
-    // Update is called once per frame
-    void Update () {
-        if (!hitPoint0 && videoPlayer.frame > 730)
-        {
-            InitializeDialogOptions("Yes", "No", "Are you kidding!?");
-            videoPlayer.frame = 500;
-        }
-        if (!hitPoint1 && videoPlayer.frame > 1500)
-        {
-            hitPoint1 = true;
-            InitializePuzzle();
-        }
+    void Update()
+    {
+        HandleKeystrokes();
 
-		if (Input.GetKeyDown(KeyCode.W))
+        HandleHitPoints();
+    }
+
+    void HandleHitPoints()
+    {
+        switch (CurrentStage)
         {
-            videoPlayer.targetCameraAlpha += 0.1f;
+            case 0:
+                Stage0();
+                break;
+            case 1:
+                Stage1();
+                break;
+            case 2:
+                Stage2();
+                break;
+            case 3:
+                Stage3();
+                break;
+            case 4:
+                Stage4();
+                break;
+            case 5:
+                Stage5();
+                break;
+            case 6:
+                Stage6();
+                break;
+            case 7:
+                Stage7();
+                break;
+        }
+    }
+
+    void Stage0()
+    {
+        switch (CurrentDialogLevel)
+        {
+            case 0:
+                SetNewVideo(Ref.VideoFiles[0]);
+                CurrentDialogLevel++;
+                break;
+            case 1:
+                if (VideoPlayer.frame > 730)
+                {
+                    InitializeDialogOptions(Ref.DialogOptions[0]);
+                    CurrentDialogLevel++;
+                }
+                break;
+            case 2:
+                if (OptionASelected || OptionBSelected || OptionCSelected || OptionDSelected)
+                {
+                    OptionButtons.ForEach(optionButton => optionButton.SetActive(false));
+                    CurrentDialogLevel++;
+                }
+                else if (VideoPlayer.frame > 850) SetVideoToFrame(730);
+                break;
+            case 3:
+                SetNewVideo(Ref.VideoFiles[2]);
+                SetVideoToFrame(VideoPlayer.frame + 600); // temporary
+                CurrentDialogLevel++;
+                break;
+            case 4:
+                if (VideoPlayer.frame > 300)
+                {
+                    InitializePuzzle();
+                    Stopwatch.Start();
+                    CurrentDialogLevel++;
+                }
+                break;
+            case 5:
+                if (Stopwatch.ElapsedMilliseconds > 2000)
+                {
+                    Stopwatch.Stop();
+                    Stopwatch.Reset();
+                    PuzzleCompleted();
+                    SetNewVideo(Ref.VideoFiles[1]);
+                    CurrentDialogLevel++;
+                }
+                break;
+            case 6:
+                if (OptionASelected)
+                {
+                    InitializeDialogOptions(new List<string> { "Great", "Job", "You", "Win!" });
+                    OptionASelected = false;
+                    CurrentDialogLevel++;
+
+                }
+                else if (OptionBSelected || OptionCSelected || OptionDSelected)
+                {
+                    InitializeDialogOptions(new List<string> { "Wrong", "Choice", "You", "Lose!" });
+                    new List<bool>() { OptionBSelected, OptionCSelected, OptionDSelected }.ForEach(x => x = false);
+                    CurrentDialogLevel++;
+                }
+                break;
+            default:
+                CurrentStage++;
+                CurrentDialogLevel = 0;
+                break;
+        }
+    }
+
+    void Stage1()
+    {
+    }
+
+    void Stage2()
+    {
+    }
+
+    void Stage3()
+    {
+    }
+
+    void Stage4()
+    {
+    }
+
+    void Stage5()
+    {
+    }
+
+    void Stage6()
+    {
+    }
+
+    void Stage7()
+    {
+    }
+
+    void SetNewVideo(string newVideoUrl)
+    {
+        PauseVideo();
+        VideoPlayer.url = newVideoUrl;
+        SetVideoToFrame(0);
+        PlayVideo();
+    }
+
+    void InitializeVideoPlayer()
+    {
+        var camera = GameObject.Find("Main Camera");
+        VideoPlayer = camera.AddComponent<VideoPlayer>();
+        VideoPlayer.playOnAwake = false;
+        VideoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+        VideoPlayer.targetCameraAlpha = 0.99f;
+        VideoPlayer.isLooping = true;
+
+        AudioPlayer = camera.AddComponent<AudioSource>();
+        AudioPlayer.clip = Resources.Load<AudioClip>("audio3");
+    }
+
+    void SetVideoToFrame(long frame)
+    {
+        VideoPlayer.frame = frame;
+        AudioPlayer.time = frame / VideoPlayer.frameRate;
+    }
+
+    void PlayVideo()
+    {
+        VideoPlayer.Play();
+        AudioPlayer.Play();
+    }
+
+    void PauseVideo()
+    {
+        VideoPlayer.Pause();
+        AudioPlayer.Pause();
+    }
+
+    void HandleKeystrokes()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            VideoPlayer.targetCameraAlpha += 0.1f;
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            videoPlayer.targetCameraAlpha -= 0.1f;
+            VideoPlayer.targetCameraAlpha -= 0.1f;
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (videoPlayer.isPlaying)
-                videoPlayer.Pause();
-            else videoPlayer.Play();
+            if (VideoPlayer.isPlaying)
+                PauseVideo();
+            else VideoPlayer.Play();
         }
 
         if (Input.GetKeyDown(KeyCode.A))
@@ -76,21 +242,35 @@ public class Camera : MonoBehaviour {
         {
             PuzzleCompleted();
         }
-	}
+    }
 
+    bool OptionASelected = false;
     void OptionAClicked()
     {
-        hitPoint0 = true;
-        OptionButtons.ForEach(optionButton =>
-        {
-            optionButton.SetActive(false);
-        });
+        OptionASelected = true;
     }
-    
-    void InitializeDialogOptions(params string[] options)
+
+    bool OptionBSelected = false;
+    void OptionBClicked()
     {
-        int i;
-        for (i = 0; i < options.Length; i++)
+        OptionBSelected = true;
+    }
+
+    bool OptionCSelected = false;
+    void OptionCClicked()
+    {
+        OptionCSelected = true;
+    }
+
+    bool OptionDSelected = false;
+    void OptionDClicked()
+    {
+        OptionDSelected = true;
+    }
+
+    void InitializeDialogOptions(List<string> options)
+    {
+        for (int i = 0; i < options.Count; i++)
         {
             OptionButtons[i].SetActive(true);
             OptionButtons[i].GetComponentInChildren<Text>().text = options[i];
@@ -102,7 +282,7 @@ public class Camera : MonoBehaviour {
         this.fadeVideoAlpha = 0.1f;
         this.fadeVideoCancelWhenReachedAlpha = 0.1f;
         InvokeRepeating("FadeVideoAlpha", 0, 0.1f);
-        this.videoPlayer.Pause();
+        PauseVideo();
     }
 
     void PuzzleCompleted()
@@ -110,22 +290,21 @@ public class Camera : MonoBehaviour {
         this.addVideoAlpha = 0.1f;
         this.addVideoCancelWhenReachedAlpha = 1.0f;
         InvokeRepeating("AddVideoAlpha", 0, 0.1f);
-        this.videoPlayer.Play();
     }
 
     private float addVideoAlpha;
     private float addVideoCancelWhenReachedAlpha;
     void AddVideoAlpha()
     {
-        var remainingAlphaToChange = addVideoCancelWhenReachedAlpha - this.videoPlayer.targetCameraAlpha;
+        var remainingAlphaToChange = addVideoCancelWhenReachedAlpha - this.VideoPlayer.targetCameraAlpha;
         if (remainingAlphaToChange < addVideoAlpha)
         {
-            this.videoPlayer.targetCameraAlpha = addVideoCancelWhenReachedAlpha;
+            this.VideoPlayer.targetCameraAlpha = addVideoCancelWhenReachedAlpha;
             CancelInvoke("AddVideoAlpha");
         }
         else
         {
-            this.videoPlayer.targetCameraAlpha += addVideoAlpha;
+            this.VideoPlayer.targetCameraAlpha += addVideoAlpha;
         }
     }
 
@@ -133,15 +312,15 @@ public class Camera : MonoBehaviour {
     private float fadeVideoCancelWhenReachedAlpha;
     void FadeVideoAlpha()
     {
-        var remainingAlphaToChange = this.videoPlayer.targetCameraAlpha - fadeVideoCancelWhenReachedAlpha;
+        var remainingAlphaToChange = this.VideoPlayer.targetCameraAlpha - fadeVideoCancelWhenReachedAlpha;
         if (remainingAlphaToChange < fadeVideoAlpha)
         {
-            this.videoPlayer.targetCameraAlpha = fadeVideoCancelWhenReachedAlpha;
+            this.VideoPlayer.targetCameraAlpha = fadeVideoCancelWhenReachedAlpha;
             CancelInvoke("FadeVideoAlpha");
         }
         else
         {
-            this.videoPlayer.targetCameraAlpha -= fadeVideoAlpha;
+            this.VideoPlayer.targetCameraAlpha -= fadeVideoAlpha;
         }
     }
 }
---
+++ Assets/Ref.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public static class Ref
+    {
+        public static List<List<string>> DialogOptions = new List<List<string>>
+        {
+            new List<string>{ "Yes", "No", "Are you Kidding me?!" },
+
+        };
+
+        public static List<string> VideoFiles = new List<string>
+        {
+            Application.dataPath + "/video3.mp4",
+            Application.dataPath + "/video.mp4",
+            Application.dataPath + "/video2.mp4",
+
+        };
+
+        public static List<string> AudioFiles = new List<string>
+        {
+            Application.dataPath + "/audio.mp3",
+
+        };
+        
+    }
+}
